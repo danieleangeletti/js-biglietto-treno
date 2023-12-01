ee1eb7ad97a3ed9a61dfd28409bb68bb5043f881
danieleangeletti <angelettidaniele@gmail.com>
2023-12-01 14:30:30 +0100

    inizializzazione progetto e scaletta

---
+++ javascript/scripts.js
@@ -0,0 +1,24 @@
+/*
+    Calcolo del prezzo del biglietto del treno
+
+    Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+    Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+    il prezzo del biglietto è definito in base ai km (0.21 € al km)
+    va applicato uno sconto del 20% per i minorenni
+    va applicato uno sconto del 40% per gli over 65.
+    L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+*/
+
+/*
+
+Qual è il problema? Calcolare il prezzo totale del viaggio.
+
+- Due prompt: n chilometri e n anni;
+- Costo del viaggio: 0.21€ * km;
+- Se n anni < 18: costo del viaggio: costo del viaggio - 20%;
+- Se n anni > 65: costo del viaggio: costo del viaggio - 40%;
+
+note: il prezzo è da indicare con massimo due decimali
+
+*/
+
